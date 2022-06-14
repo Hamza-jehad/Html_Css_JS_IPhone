9c5295b6c8ea052115ef913aae1342dfd468ce6c
Hamza-jehad <yasamer569@gmail.com>
2022-06-14 22:14:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function phones(srccc){
+    img.src = srccc;
+};
+
+function bgColors(color){
+    container.style.background = color;
+}
